4a2226773690034d06763fb0cd3b8efef742401c
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-04-23 10:39:13 +0800

    有效的字母异位词

---
+++ valid-anagram/valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
