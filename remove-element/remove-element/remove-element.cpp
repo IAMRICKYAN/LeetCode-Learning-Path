aa659580fcedfa382f0f280156aa9adead657dad
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-05-11 16:53:13 +0800

    移除元素

---
+++ remove-element/remove-element/remove-element.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int right = nums.size();
+        int left = 0;
+        while (left < right)
+        {
+            if (nums[left] == val)
+            {
+                nums[left] = nums[right-1];
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return left;
+
+    }
+};
