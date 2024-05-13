dd6d9a45887ecf765ffc9799d4dc6df3450d2233
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-05-13 20:21:06 +0800

    多数元素

--- majority-element/majority-element/majority-element.cpp
+++ majority-element/majority-element/majority-element.cpp
@@ -32,3 +32,58 @@ public:
     }
 };
 
+//分治
+class Solution {
+    int count_in_range(vector<int>& nums, int target, int lo, int hi) {
+        int count = 0;
+        for (int i = lo; i <= hi; ++i)
+            if (nums[i] == target)
+                ++count;
+        return count;
+    }
+    int majority_element_rec(vector<int>& nums, int lo, int hi) {
+        if (lo == hi)
+            return nums[lo];
+        int mid = (lo + hi) / 2;
+        int left_majority = majority_element_rec(nums, lo, mid);
+        int right_majority = majority_element_rec(nums, mid + 1, hi);
+        if (count_in_range(nums, left_majority, lo, hi) > (hi - lo + 1) / 2)
+            return left_majority;
+        if (count_in_range(nums, right_majority, lo, hi) > (hi - lo + 1) / 2)
+            return right_majority;
+        return -1;
+    }
+public:
+    int majorityElement(vector<int>& nums) {
+        return majority_element_rec(nums, 0, nums.size() - 1);
+    }
+};
+
+
+//投票算法
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = -1;
+
+        for (auto& num : nums)
+        {
+            if (candidate == num)
+            {
+                count++;
+            }
+            else
+            {
+                if (--count < 0)
+                {
+                    candidate = num;
+                    count = 1;
+                }
+
+            }
+        }
+
+        return candidate;
+    }
+};
