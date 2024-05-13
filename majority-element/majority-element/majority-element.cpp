a0e26765a6ac1f574be6eb499fca37df96525f5f
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-05-13 09:20:41 +0800

    多数元素

---
+++ majority-element/majority-element/majority-element.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+//สýัง
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
+
+
+//นþฯฃฑํ
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        int majority = 0, cnt = 0;
+        for (int num : nums) {
+            ++counts[num];
+            if (counts[num] > cnt) {
+                majority = num;
+                cnt = counts[num];
+            }
+        }
+        return majority;
+    }
+};
+
