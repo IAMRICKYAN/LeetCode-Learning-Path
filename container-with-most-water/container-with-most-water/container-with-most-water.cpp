84cbc8b5117a37b47991f172713937774e1a8d9f
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-05-23 09:20:42 +0800

    盛水最多的容器

---
+++ container-with-most-water/container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int size = height.size();
+
+        int leftmax = 0;
+        int rightmax = 0;
+
+        int left = 0;
+        int right = size - 1;
+
+        
+
+        return ans;
+    }
+};
