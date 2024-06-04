ccf970bf1f44ff854fe805ea1be1806e13d71b13
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-06-04 11:46:27 +0800

    环形链表

---
+++ linked-list-cycle/linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (slow != fast) {
+            if (fast == nullptr || fast->next == nullptr) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return true;
+    }
+};
