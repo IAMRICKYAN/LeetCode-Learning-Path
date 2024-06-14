74d84bac63d664b665d0832f4007029fc1f182cd
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-06-14 08:46:49 +0800

    路径总和

---
+++ path-sum/path-sum/path-sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+
+        if (root == nullptr)
+            return false;
+
+        targetSum -= root->val;
+
+        if (root->left == root->right)
+            return targetSum == 0;
+
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    }
+
+  
+};
