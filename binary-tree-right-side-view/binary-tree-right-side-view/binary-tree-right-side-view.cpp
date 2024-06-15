7fd46b940870de0a5defa7a8b39b079caba1a749
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-06-15 09:53:58 +0800

    二叉树的右视图

---
+++ binary-tree-right-side-view/binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+
+public:
+    vector<int> ans;
+
+    vector<int> rightSideView(TreeNode* root) {
+
+        dfs(root, 0);
+        return ans;
+
+    }
+
+    void dfs(TreeNode* node, int depth)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+
+        if (depth == ans.size())
+        {
+            ans.emplace_back(node->val);
+        }
+
+        dfs(node->right, depth + 1);
+        dfs(node->left, depth + 1);
+
+
+        return;
+    }
+
+};
