39f9db8e80f02a338e8e2912bf3842ce1301a7c6
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-06-16 10:17:56 +0800

    二叉搜索树中第K小的元素

---
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int res;
+    int K;
+
+
+    int kthSmallest(TreeNode* root, int k) {
+        
+        this->K = k;
+        res = 1;
+        dfs(root);
+
+        return res;
+    }
+
+    void dfs(TreeNode* root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+
+        dfs(root->left);
+
+        if (K == 0) return;
+        if (--K == 0) res = root->val;
+
+        dfs(root->right);
+    }
+   
+};
