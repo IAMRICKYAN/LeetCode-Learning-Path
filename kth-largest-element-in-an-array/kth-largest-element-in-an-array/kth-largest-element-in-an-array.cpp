9dcd7127333dc9dd128a5c6f7f8252aa558884dd
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-08-02 13:35:33 +0800

    数组中的第K个最大元素

---
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (auto& num : nums)
+        {
+            pq.emplace(num);
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+
+        return pq.top();
+    }
+};
