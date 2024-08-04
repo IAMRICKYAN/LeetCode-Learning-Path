536ab5e77dc9f280e4d88f1cc332d71160c16166
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-08-04 21:37:25 +0800

    数据流的中位数

---
+++ find-median-from-data-stream/find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>, greater<int>> A; // 小顶堆，保存较大的一半
+    priority_queue<int, vector<int>, less<int>> B; // 大顶堆，保存较小的一半
+    MedianFinder() { }
+    void addNum(int num) {
+        if (A.size() != B.size()) {
+            A.push(num);
+            B.push(A.top());
+            A.pop();
+        }
+        else {
+            B.push(num);
+            A.push(B.top());
+            B.pop();
+        }
+    }
+    double findMedian() {
+        return A.size() != B.size() ? A.top() : (A.top() + B.top()) / 2.0;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
