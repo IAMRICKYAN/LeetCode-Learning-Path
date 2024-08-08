4d8eb263065a54f49fb9d0775d24fde3913fe974
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-08-08 18:00:17 +0800

    只出现一次的数字

---
+++ single-number/single-number/single-number.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int x : nums)
+        {
+            ans ^= x;
+        }
+
+        return ans;
+    }
+};
