ae0f59ae09c2c0e7329c5632504467b3cc39d6e3
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-08-10 10:43:24 +0800

    数字范围按位与

---
+++ bitwise-and-of-numbers-range/bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+
+        int cnt = 0;
+
+        while (left != right)
+        {
+            cnt++;
+            left >>= 1;
+            right >>= 1;
+        }
+
+
+        return left << cnt;
+    }
+};
